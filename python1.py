acb38a00845f0dd9106b5c2accfad554368fe42a
Harshitha-Nuka <harshithanuka@gmail.com>
2025-03-11 17:09:46 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,77 @@
+#1
+N = int(input("Enter the value of N:"))
+
+# Initialize the variables 'count' and 'total'
+count = N
+total = 0
+
+while count:
+    total += count
+    count -= 1
+
+# Prints the sum of N natural numbers.
+print("The sum of the first", N, "natural numbers is:", total)
+#2
+
+
+# To take input from the user
+num = int(input("Enter a number: "))
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+#3
+# Python program to find the largest number among the three input numbers
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest) 
+
+# 4 Python program to find the factorial of a number provided by the user.
+
+# To take input from the user
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
+#5 Python program to check if the input number is odd or even.
+
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
